4468c2be3b2abca14d98c529b616967764ee1ccb
MayurDSutar <mayursutar2018@gmail.com>
2023-11-30 12:53:46 +0530

    Done some small changes

--- script.js
+++ script.js
@@ -7,10 +7,15 @@ buttons.forEach(button =>{
      else if( button.innerText=='AC')
      button.addEventListener('click', ()=>{
       input.innerText='0';
+      output.innerText=null;
+    });else if(button.innerText=='=')
+    button.addEventListener('click', ()=>{
+        output.innerText=eval(input.innerText);
     })
 })
 function distoinput(e){
      let btn=e.target;
+     if(input.innerText=='0')input.innerText=null;
     input.innerText= input.innerText+ btn.innerText ;
 }
 
