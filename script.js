0a078a18e2bcb0faeb2fe12ae155e6e1793898d6
MayurDSutar <mayursutar2018@gmail.com>
2023-12-19 18:36:11 +0530

    Addsome functionality

--- script.js
+++ script.js
@@ -7,15 +7,28 @@ buttons.forEach(button =>{
      else if( button.innerText=='AC')
      button.addEventListener('click', ()=>{
       input.innerText='0';
-      output.innerText=null;
-    });else if(button.innerText=='=')
+      output.innerText='';
+    }); else if(button.innerText=='=')
     button.addEventListener('click', ()=>{
         output.innerText=eval(input.innerText);
+    });else if(button.innerText==='DEL')
+    button.addEventListener('click',()=>{
+        input.innerText=input.innerText.substring(0,(input.innerText.length)-1);
     })
+
 })
 function distoinput(e){
      let btn=e.target;
-     if(input.innerText=='0')input.innerText=null;
+     if(input.innerText=='0')input.innerText='';
     input.innerText= input.innerText+ btn.innerText ;
 }
-
+document.addEventListener('keydown',(e)=>{
+    if(!isNaN(e.key) || ['+','-','/','*','%'].includes(e.key)){
+        output.innerText='';
+        if(input.innerText=='0')input.innerText='';
+        input.innerText+=e.key;
+    }else if(e.key==='Enter'|| e.key==='='){
+        output.innerText=eval(input.innerText);
+        input.innerText=output.innerText;
+    }
+})
